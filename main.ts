e7906e25e56e1d66921769593f9bc99cf8c289ff
Br6ndon <69922049+Br6ndon@users.noreply.github.com>
2020-08-20 11:05:13 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -23,8 +23,22 @@ info.setScore(0)
 info.changeScoreBy(5)
 info.startCountdown(20)
 info.setLife(2)
-let mySprite2 = 0
-mySprite2.setPosition(36, 30)
-info.player2.changeLifeBy(1)
-scene.cameraFollowSprite(mySprite)
-controller.moveSprite(sprites.create(mySprite2, SpriteKind.Enemy), 150, 200)
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . . . c c f f f . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . . c d d b b f . . . . . . . . . . . . . 
+    . . . . . . . . f f f f f f f f f d d b b f . . . . . . . . . . . . . . 
+    . . . . . . . f b b b b b b b b b f c b c f . . . . . . . . . . . . . . 
+    . . . . . . . f b b c 1 1 1 b f f b b c c f f f f . . . . . . . . . . . 
+    . . . . . . . f b 1 1 1 1 1 1 f f b b b b b c c c f f . . . . c c c c c 
+    . . . . . . . . f 1 c c 3 3 1 1 b b c b c b b c c c c f . . c d b b b c 
+    . . . . . . . . f c c 1 3 1 c 1 b b b c b c b c c c c c f c d d b b c . 
+    . . . . . . . . . f 1 1 1 1 1 1 b b b c b c b c c c c c c b d b b f . . 
+    . . . . . . . . . f 1 1 1 1 1 1 1 b b b b b c c c c c c c c b b c f . . 
+    . . . . . . . . . . f 1 1 1 1 1 1 b b b b c c c c c c c f f b c c f . . 
+    . . . . . . . . . . . c 1 1 1 1 c b b b c c c c c b d b c . f b b c f . 
+    . . . . . . . . . . . . c c 1 1 c b b b f d d d d d d c . . . f b b f . 
+    . . . . . . . . . . . . . . c f f b d b b f d d d c c . . . . . f b b f 
+    . . . . . . . . . . . . . . . . . f b d b b f c c . . . . . . . . f f f 
+    . . . . . . . . . . . . . . . . . . f f f f f . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
+mySprite2.setPosition(28, 46)
